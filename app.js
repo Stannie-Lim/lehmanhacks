763af2b7610ebced3b11ad6dbdba541cfdeb1bf8
Stanley <lims95@yahoo.com>
2019-11-15 21:11:54 -0500

    almost done with auth

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-var app = express();
+var express               = require("express"),
+    bodyParser            = require("body-parser"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    LocalStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    User                  = require("./models/user"),
+    app                   = express();
+
+
+mongoose.connect("mongodb://localhost/social_app");
+
+app.use(require("express-session")({
+    secret: "we must kill this love its sad but its true",
+    resave: false,
+    saveUninitialized: false
+}));
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.set("view engine", "ejs");
 
-
+//########## routes
 app.get("/", function(req, res){
-    res.render("home.ejs");
+    res.render("home");
 });
 
-app.get("/testing", function(req, res){
-    res.send("sadas");
+app.get("/secret", function(req, res){
+    res.render("secret");
 });
 
-app.listen(process.env.PORT, process.env.IP);
+//############ user authentication
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+
+
+
+//app.listen(process.env.PORT, process.env.IP);
+
+app.listen(3000, process.env.IP);
