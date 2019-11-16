9e50202f49831eb825e09f1662b8a0aaab711ff9
Stanley <lims95@yahoo.com>
2019-11-15 23:03:17 -0500

    added navbar, login and register

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var express               = require("express"),
     app                   = express();
 
 
-//mongoose.connect("mongodb://localhost/social_app");
+mongoose.connect("mongodb://localhost/social_app");
 
 app.use(require("express-session")({
     secret: "we must kill this love its sad but its true",
@@ -21,6 +21,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -40,7 +41,32 @@ app.get("/register", function(req, res){
     res.render("register");
 });
 
+app.post("/register", function(req, res){
+    req.body.username
+    req.body.password 
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret");
+        });
+    });
+});
 
-//app.listen(process.env.PORT, process.env.IP);
+//####### login
+app.get("/login", function(req, res){
+    res.render("login");
+});
 
-app.listen(3000, process.env.IP);
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"   
+    }), function(req, res){
+
+});
+
+app.listen(process.env.PORT, process.env.IP);
+
+//app.listen(3000, process.env.IP);
